aebd4a8180c999e00231c2150704b7cc313904d1
Wilson Wong <wilson.ka.wong@gmail.com>
2021-11-25 17:00:47 -0600

    Fixed bug related to the 0 button

--- client/src/calculator.js
+++ client/src/calculator.js
@@ -6,6 +6,7 @@ let xString = '';
 let yString = '';
 let typingX = true;
 let typingY = false;
+let typingDecimal = false;
 let calcSquareRoot = false;
 let operator = '';
 let result = 0;
@@ -15,9 +16,21 @@ const numbers = document.querySelector('div.numbers');
 const numButtons = document.querySelectorAll('button.numButton');
 const equalButton = document.querySelector('button.equalButton');
 const plusMinusButton = document.querySelector('button.plusMinusButton');
+const decimalButton = document.querySelector('button.decimalButton');
 const clearButton = document.querySelector('button.clearButton');
 const backspaceButton = document.querySelector('button.backspaceButton');
 const operatorButtons = document.querySelectorAll('button.operatorButton');
+
+const checkNumberRange = () => {
+  if (x > Number.MAX_SAFE_INTEGER) {
+    x = Infinity;
+    xString = '∞';
+  }
+  if (y > Number.MAX_SAFE_INTEGER) {
+    y = Infinity;
+    yString = '∞';
+  }
+};
 
 // BUTTON CLICK HANDLERS
 const handleNumPress = (e) => {
@@ -25,31 +38,34 @@ const handleNumPress = (e) => {
     if (typingX) {
       xString += e.target.textContent;
       x = Number(xString);
-      console.log('x is ', x);
-      numbers.textContent = x;
+      checkNumberRange();
+      numbers.textContent = xString;
     }
     if (typingY) {
       yString += e.target.textContent;
       y = Number(yString);
-      numbers.textContent = xString + ' ' + operator + ' ' + yString;
+      checkNumberRange();
+      numbers.textContent = `${xString} ${operator} ${yString}`;
     }
   }
   if (e.type === 'keydown') {
     if (typingX) {
       xString += e.key;
       x = Number(xString);
-      numbers.textContent = x;
+      checkNumberRange();
+      numbers.textContent = xString;
     }
     if (typingY) {
       yString += e.key;
       y = Number(yString);
-      numbers.textContent = xString + ' ' + operator + ' ' + yString;
+      checkNumberRange();
+      numbers.textContent = `${xString} ${operator} ${yString}`;
     }
   }
 };
 numButtons.forEach((numButton) => { numButton.addEventListener('click', handleNumPress); });
 document.addEventListener('keydown', (e) => {
-  if (Number(e.key)) { handleNumPress(e); }
+  if (isFinite(e.key)) { handleNumPress(e); }
 });
 
 const handleOperatorPress = (e) => {
@@ -59,16 +75,15 @@ const handleOperatorPress = (e) => {
   if (e.type === 'keydown') {
     operator = e.key;
   }
-  if (operator === '√') {
-    calcSquareRoot = true;
-  }
   if (typingX) {
     typingX = false;
     typingY = true;
-    if (operator !== '!') {
-      numbers.textContent = xString + ' ' + operator + ' ';
-    } else {
+    if (operator === '!') {
       numbers.textContent = xString + operator;
+    } else if (operator === '√') {
+      numbers.textContent = `sqrt(${xString})`;
+    } else {
+      numbers.textContent = `${xString} ${operator} `;
     }
   }
   if (typingY) {
@@ -85,28 +100,47 @@ document.addEventListener('keydown', (e) => {
 
 const handlePlusMinusPress = () => {
   if (typingX) {
-    xString = '-' + xString;
+    xString = `-${xString}`;
     x = -x;
-    numbers.textContent = x;
+    numbers.textContent = xString;
   }
   if (typingY) {
-    yString = '-' + yString;
+    yString = `-${yString}`;
     y = -y;
-    numbers.textContent = y;
+    numbers.textContent = yString;
   }
 };
 plusMinusButton.addEventListener('click', handlePlusMinusPress);
+
+const handleDecimalPress = () => {
+  if (!typingDecimal) {
+    typingDecimal = true;
+    if (typingX) {
+      xString += '.';
+      x = Number(xString);
+      checkNumberRange();
+      console.log(x);
+      numbers.textContent = xString;
+    }
+    if (typingY) {
+
+    }
+  }
+};
+decimalButton.addEventListener('click', handleDecimalPress);
+document.addEventListener('keydown', (e) => {
+  if (e.key === '.') { handleDecimalPress(); }
+});
 
 const handleEqualPress = () => {
   if (calcSquareRoot) {
     result = operate(operator, x);
     x = result;
     xString = x.toString();
-    numbers.textContent = x;
+    numbers.textContent = xString;
   }
   if (typingX) {
-    numbers.textContent = x;
-    // resultDisplayed = true;
+    numbers.textContent = xString;
   }
   if (typingY) {
     // Set x equal to result, display x
@@ -114,7 +148,7 @@ const handleEqualPress = () => {
     x = result;
     xString = x.toString();
     typingX = true;
-    numbers.textContent = x;
+    numbers.textContent = xString;
     // Reset y
     y = 0;
     yString = '';
@@ -130,12 +164,12 @@ const handleBackspacePress = () => {
   if (typingX) {
     xString = xString.slice(0, xString.length - 1);
     x = Number(xString);
-    numbers.textContent = x;
+    numbers.textContent = xString;
   }
   if (typingY) {
     yString = yString.slice(0, yString.length - 1);
     y = Number(yString);
-    numbers.textContent = y;
+    numbers.textContent = yString;
   }
 };
 backspaceButton.addEventListener('click', handleBackspacePress);
@@ -150,9 +184,10 @@ const handleClearPress = () => {
   y = 0;
   typingX = true;
   typingY = false;
-  numbers.textContent = x;
+  typingDecimal = false;
+  numbers.textContent = xString;
 };
 clearButton.addEventListener('click', handleClearPress);
 
 // Initialize display
-numbers.textContent = x;
+numbers.textContent = xString;
